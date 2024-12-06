eee6e0e109971434876bcbec2374becfc85a21f8
Cyl1021 <liangchengyou27@gmail.com>
2024-12-06 16:16:08 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://cyl1021.github.io/frog11324142/model.json';
 
 
 function preload() {
